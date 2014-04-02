b04551734c45c32e977661eff55d11032e982231
Ryan Snell <ryansnell@me.com>
2014-04-02 00:26:49 -0400

    Modified layout and updated images

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<title>Penn State Classroom Availability</title>
+	</head>
+	<body>
+		<div class="container" style="margin-top:50px;">
+
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	require_once('includes/header.php');
 	error_reporting(-1);
 
 	$m = new MongoClient();
@@ -11,11 +12,17 @@
 	$cursor->sort(array('classroomName' => 1));
 
 	foreach($cursor as $document) {
-		echo $document["classroomName"]  . " ";
-		if(!isset($document["picfile"]))
+		echo '<div class="row" style="margin-bottom:10px;">';
+		echo '<div class="col-md-3">';
+		if(isset($document["picfile"]))
 		{
-			echo $document["_id"];
+			echo '<img src="' . $document["picfile"] . '" class="img-rounded" width="250" />';
 		}
-		echo "<br/>";
+		echo '</div>';
+		echo '<div class="col-md-9">';
+		echo '<h4>' . $document["classroomName"] . '</h4>';
+		echo '</div>';
+		echo '</div>';
 	}
+	require_once('includes/footer.php');
 ?>
